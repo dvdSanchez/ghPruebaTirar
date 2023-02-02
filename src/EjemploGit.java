106beed6b13a9c67aab50d378943f64254f9d433
David Sanchez Bautista <sanchezd@globalhitss.com>
2023-02-02 07:06:15 -0600

    Se agregó la clase persona y se invoco en el main

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,6 +4,10 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("DSB");
+		Persona p = new Persona();
+		p.setNombre("David");
+		System.out.println(p.getNombre());
+		
 	}
 
 }
---
+++ src/Persona.java
@@ -0,0 +1,15 @@
+
+public class Persona {
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
